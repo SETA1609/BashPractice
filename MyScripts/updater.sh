435c7d4550a651d632a6b6739e3d1e08add0cf02
SETA1609 <sebastian.stp16@gmail.com>
2023-08-25 22:01:15 +0200

    ft: 3nd iteration from thw updater

--- MyScripts/updater.sh
+++ MyScripts/updater.sh
@@ -8,20 +8,7 @@ then
 	sudo pacman -Syu
 fi
 
-if grep -q "Ubuntu" $releaseFile
-then
-	#if debian
-	sudo apt update
-	echo "would you like to distro-upgrade? y/n"
-	read answer
-	case $answer in
-		y) sudo apt dist-upgrade;;
-		n) echo "Thank you :3";;
-		*) echo "Wrong input";;
-esac
-fi
-
-if grep -q "Pop" $releaseFile
+if grep -q "Pop" $releaseFile || grep -q "Debian" $releaseFile || grep -q "Ubuntu" $releaseFile
 then
 	#if debian
 	sudo apt update
