c0844d813720a0ac69b286ea56c0b1d77579e6f4
SETA1609 <sebastian.stp16@gmail.com>
2023-08-25 21:54:49 +0200

    ft: 2nd iteration from thw updater

--- MyScripts/updater.sh
+++ MyScripts/updater.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-if [ -d /etc/pacman.d ]
+releaseFile=/etc/os-release
+
+if grep -q "Arch" $releaseFile
 then
 	#if arch
 	sudo pacman -Syu
 fi
 
-if [ -d /etc/apt ]
+if grep -q "Ubuntu" $releaseFile
 then
 	#if debian
 	sudo apt update
 	echo "would you like to distro-upgrade? y/n"
 	read answer
 	case $answer in
-		y) sudo apt dist-upgrade
-		n) echo "Thank you :3"
-		*) echo "Wrong input"
+		y) sudo apt dist-upgrade;;
+		n) echo "Thank you :3";;
+		*) echo "Wrong input";;
+esac
+fi
+
+if grep -q "Pop" $releaseFile
+then
+	#if debian
+	sudo apt update
+	echo "would you like to distro-upgrade? y/n"
+	read answer
+	case $answer in
+		y) sudo apt dist-upgrade;;
+		n) echo "Thank you :3";;
+		*) echo "Wrong input";;
 esac
 fi
