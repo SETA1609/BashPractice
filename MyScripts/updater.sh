404f7575ba08c4f5f02312798b2953ef285b71d3
SETA1609 <sebastian.stp16@gmail.com>
2023-08-26 21:31:07 +0200

    ft: error and normal logs were added to updater

--- MyScripts/updater.sh
+++ MyScripts/updater.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
 releaseFile=/etc/os-release
+logfile=var/log/updater.log
+errorlog=/var/log/updaterErrors.log
 
 if grep -q "Arch" $releaseFile
 then
 	#if arch
-	sudo pacman -Syu
+	sudo pacman -Syu 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0  ]
+	then
+		echo "An error occurred, please check the $errorlog file."
+	fi
 fi
 
 if grep -q "Pop" $releaseFile || grep -q "Debian" $releaseFile || grep -q "Ubuntu" $releaseFile
 then
 	#if debian
-	sudo apt update
+	sudo apt update	 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0  ]
+	then
+		echo "An error occurred, please check the $errorlog file."
+	fi
 	echo "would you like to distro-upgrade? y/n"
 	read answer
 	case $answer in
-		y) sudo apt dist-upgrade;;
+		y) sudo apt dist-upgrade 1>>$logfile 2>>$errorlog;;
 		n) echo "Thank you :3";;
 		*) echo "Wrong input";;
 esac
